81114a4cd25545a29c39bd39162e38e7d57e4168
văn vui vẻ <haytv2222@gmail.com>
2025-03-22 08:19:56 +0700

    video 28

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const Component = () => {
+  return(
+    <div>van vui ve hoc lap trinh</div>
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -17,6 +23,7 @@ function App() {
         </a>
       </div>
       <h1>van vui ve</h1>
+      <Component />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
